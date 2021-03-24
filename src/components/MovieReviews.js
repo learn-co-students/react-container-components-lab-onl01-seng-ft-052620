6f46511df0b7fe5ff4206df25f32ce0d7c4004dd
aaronfeingold <ajfeingold88@gmail.com>
2021-03-23 21:38:20 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,32 @@
 // Code MovieReviews Here
+import React from 'react'
+import { nanoid } from 'nanoid';
+
+const Review = ({byline, headline, summary_short, link}) => {
+
+  return (
+    <div key={nanoid()} className="review">
+      <a href={link.url}> {headline}</a>
+      <br/>
+      <span className="author">{byline}</span>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  )
+};
+
+
+const MovieReviews = ({reviews}) => {
+    return (
+      <div className ="review-list">
+          {reviews.map(Review)}
+      </div>
+    )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
+
+
